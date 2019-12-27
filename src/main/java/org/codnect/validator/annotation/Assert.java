0dfd65a90d74fb137a81991c2058bd3faa4353c1
Burak Koken <burakkokenn@gmail.com>
2019-12-27 17:47:11 +0200

    Added Assert annotation

---
+++ src/main/java/org/codnect/validator/annotation/Assert.java
@@ -0,0 +1,50 @@
+package org.codnect.validator.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * Created by Burak Köken on 27.12.2019.
+ */
+@Target({
+        ElementType.METHOD,
+        ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER,
+        ElementType.TYPE_USE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Assert.List.class)
+@Constraint(validatedBy = {})
+@Documented
+public @interface Assert {
+
+    String message() default "{org.codnect.validator.annotation.Assert.message}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    String value() default "";
+
+    String applyIf() default "";
+
+    Class<?>[] assertHelpers() default {};
+
+    @Target({ElementType.METHOD,
+            ElementType.FIELD,
+            ElementType.ANNOTATION_TYPE,
+            ElementType.CONSTRUCTOR,
+            ElementType.PARAMETER,
+            ElementType.TYPE_USE
+    })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        Assert[] value();
+    }
+
+}
+
