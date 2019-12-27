3b8a632840517198b30de5b1fd33ff80322faadc
Burak Koken <burakkokenn@gmail.com>
2019-12-27 21:35:56 +0200

    Implemented the method of isValid in AssertValidator.java

--- src/main/java/org/codnect/validator/AssertValidator.java
+++ src/main/java/org/codnect/validator/AssertValidator.java
@@ -1,7 +1,8 @@
 package org.codnect.validator;
 
 import org.codnect.validator.annotation.Assert;
-import org.codnect.validator.expression.ExpressionValidator;
+import org.codnect.validator.expression.*;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -33,7 +34,26 @@ public class AssertValidator extends ExpressionValidator<Assert,Object> {
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+        EvaluationContext evaluationContext = new EvaluationContextBuilder()
+                .setRootObject(value)
+                .setBeanResolver(getBeanResolver())
+                .setTypeConverter(new BooleanTypeConverter())
+                .setRegisterFunctionNaming(new StandardRegisterFunctionNaming())
+                .setAssertHelpers(assertHelpers)
+                .build();
+        if(evaluateApplyIfExpression(evaluationContext)) {
+            return evaluate(evaluationContext, expression);
+        }
         return true;
+    }
+
+    private boolean evaluate(EvaluationContext evaluationContext, Expression expression) {
+        BooleanExpressionEvaluator expressionEvaluator = new BooleanExpressionEvaluator(evaluationContext);
+        return expressionEvaluator.evaluate(expression);
+    }
+
+    private boolean evaluateApplyIfExpression(EvaluationContext evaluationContext) {
+        return applyIfExpression == null || evaluate(evaluationContext, applyIfExpression);
     }
 
 }
